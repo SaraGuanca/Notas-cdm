939fe43406c958502a272b3e327158eec49f475d
Sara <saraguanca78@gmail.com>
2024-10-18 09:21:04 -0300

    git commit-m

--- notascdm_app/views.py
+++ notascdm_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Alumno
 
 # Create your views here.
 def index(request):
     return render(request,'notascdm\index.html')
+
+def retornar_alumnos(request):
+    alumnos = Alumno.object.all()
+    if alumnos:
+        context = {
+            "listaAlumnos": alumnos
+        }
+        return render (request, 'notas.html', context)
+    return render (request, 'index.html')
